292c6ab2c0986a64a0d7d73d93503789e504ef38
tamarahadz <tamara.hadzic@gmail.com>
2026-07-29 12:49:43 -0400

    set up motif clustering script as qsub

--- Motif_clustering_Drosophila.sh
+++ Motif_clustering_Drosophila.sh
@@ -1,3 +1,44 @@
+#!/bin/bash -l
+#
+# Run this file using 'qsub template.qsub argument'
+
+# All lines starting with "#$" are SGE qsub commands
+
+# Specify a project to run under
+#$ -P wunderl
+
+# Join standard output and error to a single file
+#$ -j y
+
+# Name the file where to redirect standard output and error
+#$ -o logs/template.log
+
+# Send an email when the job finishes
+#$ -m eas
+
+# Request a large memory node, this will affect your queue time, but it's better to overestimate
+#$ -l mem_per_core=8G
+
+# Request more cores, this will affect your queue time, make sure your program supports multithreading, or it's a waste
+# -pe omp 1
+
+# Now we write the script that the compute node will work on.
+
+# load modules - MEME suite
+module load perl/5.28.1
+module load python3/3.10.12
+module load openmpi/4.1.5
+module load meme/5.5.5
+
+# source the common functionality for logging (common.sh needs to be in the same directory as this qsub)
+source ./common.sh
+
+# prerun command, imported from common.sh
+prerun
+
+# set the 'argument' variable equal to the first argument on the command line, else foo
+# default function imported from common.sh
+argument="$(default $1 foo)"
 
 #####
 # Step 1: Prepare motif databases
@@ -36,3 +77,7 @@ head -n -4 tomtom.all.txt > tomtom.all.treated.txt
 #####
 # Step 3: Hierarchically cluster motifs by similarity in R (Motif_clustering.R)
 #####
+
+# postrun command, imported from common.sh
+postrun
+
